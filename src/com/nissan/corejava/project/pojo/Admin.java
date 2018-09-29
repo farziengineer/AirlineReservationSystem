2c6af1fbcff1ee372591b78c7da17d4723242a35
farziengineer <anand.mohit001@gmail.com>
2018-09-29 14:56:39 +0530

    Added Admin class

---
+++ src/com/nissan/corejava/project/pojo/Admin.java
@@ -0,0 +1,10 @@
+package com.nissan.corejava.project.pojo;
+
+public class Admin extends User{
+
+	public Admin(int adminId, String name, String email, String password, String dob, String contact) {
+		super(adminId, name, email, password, dob, contact);
+	}
+	
+	
+}
