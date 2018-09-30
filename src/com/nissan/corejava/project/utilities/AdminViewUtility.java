b9a9893e7e0593ef6dd8542a06b8db62249b1a80
farziengineer <anand.mohit001@gmail.com>
2018-09-30 17:26:23 +0530

    Added mooore code for Admin

--- src/com/nissan/corejava/project/utilities/AdminViewUtility.java
+++ src/com/nissan/corejava/project/utilities/AdminViewUtility.java
@@ -31,14 +31,28 @@ public class AdminViewUtility {
 	public void rescheduleFlight() {
 		FlightDaoImpl flightImpl = new FlightDaoImpl();
 		flightImpl.showAllFlight();
-		System.out.println("Choose the flight id to update");
-		
-		
+		System.out.println("Choose the id of the fight you want to update");
+		Scanner input = new Scanner(System.in);
+		int flightId = input.nextInt();
+		System.out.println("Enter arrival time");
+		String arrivalTime = input.next();
+		System.out.println("Enter departure time");
+		String departureTime = input.next();
+		System.out.println("Enter the source");
+		String source = input.next();
+		System.out.println("Enter the destination");
+		String destination = input.next();
+		System.out.println("Enter airline");
+		String airline = input.next();
+		System.out.println("Enter number of seats");
+		int numOfSeats = input.nextInt();
+		System.out.println("Enter the status ");
+		int status = input.nextInt();
+		flightImpl.updateFlight(flightId, arrivalTime, departureTime, source, destination, airline, numOfSeats, status);
 		
 	}
 
 	public void cancelFlight() {
-		// TODO Auto-generated method stub
 		
 	}
 
